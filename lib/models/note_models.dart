7ee0daec4c46fd22d63aa9b08079090393bc3e7c
Sandesh Rimal <rimal.sandesh11@gmail.com>
2022-01-16 16:51:35 +0545

    Created model for notes

--- lib/models/note_models.dart
+++ lib/models/note_models.dart
@@ -1,1 +1,21 @@
+class NoteModel {
+  int id;
+  String title;
+  String body;
+  DateTime createdTime;
 
+  NoteModel(
+      {required this.id,
+      required this.title,
+      required this.body,
+      required this.createdTime});
+
+  Map<String, dynamic> toMap() {
+    return ({
+      "id": id,
+      "title": title,
+      "body": body,
+      "createdTime": createdTime,
+    });
+  }
+}
