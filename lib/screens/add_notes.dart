d9a1127c3812c82efbf96f93f75d5b89b0ea219d
Sandesh Rimal <rimal.sandesh11@gmail.com>
2022-01-17 19:47:56 +0545

    Added function to save notes

--- lib/screens/add_notes.dart
+++ lib/screens/add_notes.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/db/database_provider.dart';
+import 'package:note_app/models/note_models.dart';
 
 class AddNotes extends StatefulWidget {
   const AddNotes({Key? key}) : super(key: key);
@@ -8,6 +10,17 @@ class AddNotes extends StatefulWidget {
 }
 
 class _AddNotesState extends State<AddNotes> {
+  String title = "";
+  String body = "";
+  DateTime createdTime = DateTime.now();
+
+  TextEditingController titleController = TextEditingController();
+  TextEditingController bodyController = TextEditingController();
+
+  addNotes(NoteModel note) {
+    DatabaseProvider.db.addNewNote(note);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,24 +31,42 @@ class _AddNotesState extends State<AddNotes> {
       body: Padding(
         padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 10.0),
         child: Column(
-          children: const [
+          children: [
             TextField(
-              decoration: InputDecoration(
+              controller: titleController,
+              decoration: const InputDecoration(
                 border: InputBorder.none,
                 hintText: "Note Title",
               ),
-              style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+              style: const TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
             ),
             Expanded(
               child: TextField(
+                controller: bodyController,
                 keyboardType: TextInputType.multiline,
                 maxLines: null,
-                decoration: InputDecoration(
+                decoration: const InputDecoration(
                     border: InputBorder.none, hintText: "Your Notes"),
               ),
             ),
           ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          setState(() {
+            title = titleController.text;
+            body = bodyController.text;
+            createdTime = DateTime.now();
+          });
+          NoteModel note = NoteModel(
+              id: 1, title: title, body: body, createdTime: createdTime);
+          addNotes(note);
+          Navigator.pushNamedAndRemoveUntil(context, '/', (route) => false);
+        },
+        label: const Text("Save Note"),
+        icon: const Icon(Icons.save_outlined),
+        backgroundColor: Colors.black,
       ),
     );
   }
